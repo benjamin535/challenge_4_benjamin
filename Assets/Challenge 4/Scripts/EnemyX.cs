941ed0274054d3ada0f013f9262b634f8835d3e1
benjamin535 <b00163909@mytudublin.ie>
2024-11-22 12:32:19 +0000

    cha_4

---
+++ Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal");
+
+        // Error handling if "Player Goal" isn't found
+        if (playerGoal == null)
+        {
+            Debug.LogError("Player Goal object not found in the scene.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerGoal != null) // Only move if playerGoal is assigned
+        {
+            Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+            enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+        }
+    }
+
+    // Function to set the speed of the enemy
+    public void SetSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal" || other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Challenge 4/Scripts/SpawnManagerX.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+
+    private float spawnRangeX = 10f;
+    private float spawnZMin = 15f; // set min spawn Z
+    private float spawnZMax = 25f; // set max spawn Z
+
+    public int enemyCount;
+    public int waveCount = 1;
+    public float enemySpeed = 50f; // Initial speed of enemies
+
+    public GameObject player; 
+
+    void Update()
+    {
+        // Count enemies in the scene
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+        // Spawn a new wave if no enemies are left
+        if (enemyCount == 0)
+        {
+            SpawnEnemyWave(waveCount);
+        }
+    }
+
+    // Generate random spawn position for powerups and enemy balls
+    Vector3 GenerateSpawnPosition()
+    {
+        float xPos = Random.Range(-spawnRangeX, spawnRangeX);
+        float zPos = Random.Range(spawnZMin, spawnZMax);
+        return new Vector3(xPos, 0, zPos);
+    }
+
+    // Spawn enemy wave with increasing difficulty
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        Vector3 powerupSpawnOffset = new Vector3(0, 0, -15); // Position powerups closer to the player
+
+        // Spawn a powerup if none exists
+        if (GameObject.FindGameObjectsWithTag("Powerup").Length == 0)
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPosition() + powerupSpawnOffset, powerupPrefab.transform.rotation);
+        }
+
+        // Spawn enemies and set their speed
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+
+            // Pass the enemy speed to the enemy's script if it exists
+            EnemyX enemyScript = enemy.GetComponent<EnemyX>();
+            if (enemyScript != null)
+            {
+                enemyScript.SetSpeed(enemySpeed);
+            }
+        }
+
+        // Increase wave count and enemy speed for the next wave
+        waveCount++;
+        enemySpeed += 5f; // Increment speed each wave
+        
+        // Reset player position to start position
+        ResetPlayerPosition();
+    }
+
+    // Move player back to starting position
+    void ResetPlayerPosition()
+    {
+        if (player != null)
+        {
+            player.transform.position = new Vector3(0, 1, -7);
+            Rigidbody playerRb = player.GetComponent<Rigidbody>();
+            if (playerRb != null)
+            {
+                playerRb.velocity = Vector3.zero;
+                playerRb.angularVelocity = Vector3.zero;
+            }
+        }
+    }
+}
